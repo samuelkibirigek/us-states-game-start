0de55a69281c6343c48189d7255ddbb2bce0e84e
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-12 21:45:23 +0300

    Here I create a csv file of the states not mentioned and made some improvements to existing code

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -11,13 +11,16 @@ turtle.shape(image)
 states = pandas.read_csv("50_states.csv")
 state_list = states.state.to_list()
 
-game_is_on = True
+
 correct_states = 0
 correct_guesses = []
+states_to_learn = []
 
-while game_is_on:
+while len(correct_guesses) < 50:
     answer = screen.textinput(title="Guess the State", prompt=f"{correct_states}/50 States Correct")
     answer_state = answer.title()
+    if answer_state == "Exit":
+        break
     for any_state in state_list:
         if answer_state == any_state and answer_state not in correct_guesses:
             correct_states += 1
@@ -30,9 +33,14 @@ while game_is_on:
             state_obj.goto(x_pos, y_pos)
             state_obj.write(answer_state)
             correct_guesses.append(answer_state)
-            print(correct_guesses)
-        if answer_state == "Exit":
-            game_is_on = False
+
+
+for the_states in states["state"].to_list():
+    if the_states not in correct_guesses:
+        states_to_learn.append(the_states)
+        learning_dict = {"states": states_to_learn}
+        learning_df = pandas.DataFrame(learning_dict)
+        learning_df.to_csv("states_to_learn.csv")
 
 
 turtle.mainloop()
